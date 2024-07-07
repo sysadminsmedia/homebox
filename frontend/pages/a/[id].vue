5df237223268e7ad0cd8637e9ebbdca9c55bd20c
Katos <7927609+katosdev@users.noreply.github.com>
2024-07-07 22:25:18 +0100

    [WIP] Add LostandFound page (v1)

--- frontend/pages/a/[id].vue
+++ frontend/pages/a/[id].vue
@@ -3,6 +3,12 @@
     middleware: ["auth"],
   });
 
+  if (!ctx.isAuthorized()) {
+      toast.error("User is not logged in");
+      navigateTo("/lostandfound");
+      return;
+    }
+
   const route = useRoute();
   const assetId = computed<string>(() => route.params.id as string);
   await navigateTo("/assets/" + assetId.value, { replace: true, redirectCode: 301 });
