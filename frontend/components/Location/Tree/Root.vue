913e3719c7585e98570373906b7fb70a8ef47093
Filipe Dobreira <dobreira@gmail.com>
2024-09-22 13:48:59 +0100

    fix: add empty state to locations list

--- frontend/components/Location/Tree/Root.vue
+++ frontend/components/Location/Tree/Root.vue
@@ -11,6 +11,10 @@
 
 <template>
   <div class="root border-2 p-4">
+    <p v-if="locs.length === 0" class="text-center text-sm">
+      No locations available. Add new locations through the
+      <span class="link-primary">Create</span> button on the navigation bar.
+    </p>
     <LocationTreeNode v-for="item in locs" :key="item.id" :item="item" :tree-id="treeId" />
   </div>
 </template>
