4510712359a402c3adcafe1b46533e37e3e891f2
tonyaellie <tonya@tokia.dev>
2024-10-30 16:01:05 +0000

    feat: make 404 follow theme and add a return home page

--- frontend/pages/[...all].vue
+++ frontend/pages/[...all].vue
@@ -8,8 +8,9 @@
 </script>
 
 <template>
-  <h1 class="flex flex-col text-center font-extrabold text-blue-500">
+  <h1 class="flex flex-col text-center font-extrabold">
     <span class="text-7xl">404.</span>
     <span class="mt-5 text-5xl"> Page Not Found </span>
+    <NuxtLink to="/" class="btn mt-5 text-xl"> Return Home </NuxtLink>
   </h1>
 </template>
