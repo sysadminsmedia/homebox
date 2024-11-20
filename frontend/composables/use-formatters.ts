73ef333dfc177ddd20e3d9f9d7a07e8295b3acf0
Ádám Kleizer <adkl@boyum-it.com>
2024-11-20 09:37:52 +0100

    chore: lint fixes for use-formatters

--- frontend/composables/use-formatters.ts
+++ frontend/composables/use-formatters.ts
@@ -1,5 +1,6 @@
 import { useI18n } from "vue-i18n";
 import { format, formatDistance } from "date-fns";
+/*eslint import/namespace: ['error', { allowComputed: true }]*/
 import * as Locales from "date-fns/locale";
 
 const cache = {
@@ -29,13 +30,16 @@ export type DateTimeType = "date" | "time" | "datetime";
 
 function getLocale() {
   const t = useI18n();
-  const localeCode = t?.locale?.value as string ?? "en-US";
-  const region = localeCode.length>2 ? localeCode.substring(3) : "";
-  return (Locales[(localeCode.substring(0,2) + region) as keyof typeof Locales] ?? Locales[localeCode.substring(0,2) as keyof typeof Locales] ?? Locales.enUS);
+  const localeCode = (t?.locale?.value as string) ?? "en-US";
+  const lang = localeCode.length > 1 ? localeCode.substring(0, 2) : localeCode;
+  const region = localeCode.length > 2 ? localeCode.substring(3) : "";
+  return (
+    Locales[(lang + region) as keyof typeof Locales] ?? Locales[lang as keyof typeof Locales] ?? Locales.enUS
+  );
 }
 
 export function useLocaleTimeAgo(date: Date) {
-  return formatDistance(date, new Date(), {locale: getLocale()});
+  return formatDistance(date, new Date(), { locale: getLocale() });
 }
 
 export function fmtDate(value: string | Date, fmt: DateTimeFormat = "human", type: DateTimeType = "date"): string {
@@ -57,7 +61,7 @@ export function fmtDate(value: string | Date, fmt: DateTimeFormat = "human", typ
 
   switch (fmt) {
     case "human":
-      formatStr = "PPP"
+      formatStr = "PPP";
       break;
     case "long":
       formatStr = "PP";
