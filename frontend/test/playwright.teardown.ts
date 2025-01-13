28474158df6ba7866eecc47da18f61b30ce06a44
Matthew Kilgore <matthew@kilgore.dev>
2025-01-12 22:25:00 -0500

    Safer kill of testing dependencies

--- frontend/test/playwright.teardown.ts
+++ frontend/test/playwright.teardown.ts
@@ -3,7 +3,7 @@ import { exec } from "child_process";
 function globalTeardown() {
   if (process.env.TEST_SHUTDOWN_API_SERVER) {
     const pc = exec("pkill -SIGTERM api"); // Kill background API process
-    const fr = exec("pkill -SIGTERM node"); // Kill background Frontend process
+    const fr = exec("pkill -SIGTERM task"); // Kill background Frontend process
     pc.stdout?.on("data", (data: void) => {
       console.log(`stdout: ${data}`);
     });
