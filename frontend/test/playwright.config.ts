c2b611130185a14530260b726903b4290c2c3a0d
Matthew Kilgore <matthew@kilgore.dev>
2025-03-16 11:33:47 -0400

    Use sharding based testing

--- frontend/test/playwright.config.ts
+++ frontend/test/playwright.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 1,
-  reporter: process.env.CI ? "github" : "html",
+  reporter: process.env.CI ? "blob" : "html",
   use: {
     baseURL: process.env.E2E_BASE_URL || "http://localhost:3000",
     trace: "on-all-retries",
