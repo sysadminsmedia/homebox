sysadminsmedia/homebox

commit 912e5b231aafa85561c44943c9593d3dddc8060f

Fix footer links

backend/app/api/main.go
@@ -203,7 +203,7 @@ func run(cfg *config.Config) error {
 
 	currencies, err := currencies.CollectionCurrencies(collectFuncs...)
 	if err != nil {
-		log.Error().
+		go log.Error().
 			Err(err).
 			Msg("failed to collect currencies")
 		return err

frontend/layouts/default.vue
@@ -105,9 +105,9 @@
           <slot></slot>
           <footer v-if="status" class="bottom-0 w-full bg-base-300 pb-4 text-center text-secondary-content">
             <p class="text-center text-sm">
-              {{ $t("global.footer.version_link", { version: status.build.version, build: status.build.commit }) }}
+              <span v-html="DOMPurify.sanitize($t('global.footer.version_link', { version: status.build.version, build: status.build.commit }))"></span>
               ~
-              {{ $t("global.footer.api_link") }}
+              <span v-html="DOMPurify.sanitize($t('global.footer.api_link'))"></span>
             </p>
           </footer>
         </div>
@@ -154,6 +154,7 @@
   } from "@/components/ui/dropdown-menu";
   import { Shortcut } from "~/components/ui/shortcut";
   import { useDialog } from "~/components/ui/dialog-provider";
+  import DOMPurify from "dompurify";
 
   const { t, locale } = useI18n();
   const username = computed(() => authCtx.user?.name || "User");

frontend/lib/api/types/data-contracts.ts
@@ -11,6 +11,17 @@
  * ---------------------------------------------------------------
  */
 
+export enum MaintenanceFilterStatus {
+  MaintenanceFilterStatusScheduled = "scheduled",
+  MaintenanceFilterStatusCompleted = "completed",
+  MaintenanceFilterStatusBoth = "both",
+}
+
+export enum ItemType {
+  ItemTypeLocation = "location",
+  ItemTypeItem = "item",
+}
+
 export interface CurrenciesCurrency {
   code: string;
   local: string;
@@ -150,11 +161,6 @@ export interface ItemSummary {
   purchasePrice: number;
   quantity: number;
   updatedAt: Date | string;
-}
-
-export enum ItemType {
-  ItemTypeLocation = "location",
-  ItemTypeItem = "item",
 }
 
 export interface ItemUpdate {
@@ -305,12 +311,6 @@ export interface MaintenanceEntryWithDetails {
   itemName: string;
   name: string;
   scheduledDate: Date | string;
-}
-
-export enum MaintenanceFilterStatus {
-  MaintenanceFilterStatusScheduled = "scheduled",
-  MaintenanceFilterStatusCompleted = "completed",
-  MaintenanceFilterStatusBoth = "both",
 }
 
 export interface NotifierCreate {

commit 6a4835be5931fb00c6db9ce026ac445f0fa655e3

Fix linter issues again and an error introduced in 912e5b2

backend/app/api/main.go
@@ -4,13 +4,14 @@ import (
 	"bytes"
 	"context"
 	"fmt"
-	"github.com/google/uuid"
-	"github.com/sysadminsmedia/homebox/backend/internal/sys/analytics"
 	"net/http"
 	"os"
 	"path/filepath"
 	"strings"
 	"time"
+
+	"github.com/google/uuid"
+	"github.com/sysadminsmedia/homebox/backend/internal/sys/analytics"
 
 	atlas "ariga.io/atlas/sql/migrate"
 	"entgo.io/ent/dialect/sql/schema"
@@ -203,7 +204,7 @@ func run(cfg *config.Config) error {
 
 	currencies, err := currencies.CollectionCurrencies(collectFuncs...)
 	if err != nil {
-		go log.Error().
+		log.Error().
 			Err(err).
 			Msg("failed to collect currencies")
 		return err

frontend/pages/item/[id]/index.vue
@@ -533,7 +533,7 @@
         <header :class="{ 'mb-2': item.description }">
           <div class="flex flex-wrap items-end gap-2">
             <div
-              class="bg-secondary text-secondary-foreground mb-auto flex size-12 items-center justify-center rounded-full"
+              class="mb-auto flex size-12 items-center justify-center rounded-full bg-secondary text-secondary-foreground"
             >
               <MdiPackageVariant class="size-7" />
             </div>
@@ -698,7 +698,7 @@
               </template>
             </DetailsSection>
             <div v-else>
-              <p class="text-foreground/70 px-6 pb-4">No attachments found</p>
+              <p class="px-6 pb-4 text-foreground/70">No attachments found</p>
             </div>
           </BaseCard>
 