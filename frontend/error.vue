9b77533932b6f95067125954a0158cae7c560ba4
tonyaellie <tonya@tokia.dev>
2025-05-08 08:07:57 +0000

    fix: require error prop in error.vue and remove optional chaining for more reliable error handling

--- frontend/error.vue
+++ frontend/error.vue
@@ -3,8 +3,10 @@
   import { buttonVariants } from "@/components/ui/button";
 
   const props = defineProps({
-    // eslint-disable-next-line vue/require-default-prop
-    error: Object as () => NuxtError,
+    error: {
+      type: Object as () => NuxtError,
+      required: true,
+    },
   });
 
   console.log(props.error);
@@ -13,8 +15,8 @@
 <template>
   <main class="grid min-h-screen w-full place-items-center">
     <h1 class="flex flex-col gap-5 text-center font-extrabold">
-      <span class="text-7xl">{{ error?.statusCode }}.</span>
-      <span class="text-5xl"> {{ error?.message }} </span>
+      <span class="text-7xl">{{ error.statusCode }}.</span>
+      <span class="text-5xl"> {{ error.message }} </span>
       <NuxtLink to="/" :class="buttonVariants({ size: 'lg' })"> Return Home </NuxtLink>
     </h1>
   </main>
