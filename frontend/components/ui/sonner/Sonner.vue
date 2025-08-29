4e0dd04b42883fc5a3e8f5cc04c30136499fe538
tonyaellie <tonya@tokia.dev>
2025-08-29 22:35:12 +0100

    fix: import sonner styles

--- frontend/components/ui/sonner/Sonner.vue
+++ frontend/components/ui/sonner/Sonner.vue
@@ -1,7 +1,8 @@
 <script lang="ts" setup>
-import { Toaster as Sonner, type ToasterProps } from 'vue-sonner'
+  import { Toaster as Sonner, type ToasterProps } from "vue-sonner";
+  import "vue-sonner/style.css";
 
-const props = defineProps<ToasterProps>()
+  const props = defineProps<ToasterProps>();
 </script>
 
 <template>
@@ -12,12 +13,11 @@ const props = defineProps<ToasterProps>()
     :visible-toasts="10"
     :toast-options="{
       classes: {
-        toast: 'group toast group-[.toaster]:bg-background group-[.toaster]:text-foreground group-[.toaster]:border-border group-[.toaster]:shadow-lg',
+        toast:
+          'group toast group-[.toaster]:bg-background group-[.toaster]:text-foreground group-[.toaster]:border-border group-[.toaster]:shadow-lg',
         description: 'group-[.toast]:text-muted-foreground',
-        actionButton:
-          'group-[.toast]:bg-primary group-[.toast]:text-primary-foreground',
-        cancelButton:
-          'group-[.toast]:bg-muted group-[.toast]:text-muted-foreground',
+        actionButton: 'group-[.toast]:bg-primary group-[.toast]:text-primary-foreground',
+        cancelButton: 'group-[.toast]:bg-muted group-[.toast]:text-muted-foreground',
       },
     }"
   />
