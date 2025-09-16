b2ac6b42180815b6a103ac36f2a6382d9a32e7a4
tonyaellie <tonya@tokia.dev>
2025-09-16 10:48:38 +0100

    feat: json download for table

--- frontend/components/Item/View/table/data-table-dropdown.vue
+++ frontend/components/Item/View/table/data-table-dropdown.vue
@@ -23,7 +23,7 @@
     (e: "expand"): void;
   }>();
 
-  const download = (items: Row<ItemSummary>[], columns: Column<ItemSummary>[]) => {
+  const downloadCsv = (items: Row<ItemSummary>[], columns: Column<ItemSummary>[]) => {
     // get enabled columns
     const enabledColumns = columns.filter(c => c.id !== undefined && c.getIsVisible() && c.getCanHide()).map(c => c.id);
 
@@ -45,6 +45,35 @@
     a.remove();
     URL.revokeObjectURL(url);
   };
+
+  const downloadJson = (items: Row<ItemSummary>[], columns: Column<ItemSummary>[]) => {
+    // get enabled columns
+    const enabledColumns = columns.filter(c => c.id !== undefined && c.getIsVisible() && c.getCanHide()).map(c => c.id);
+
+    // map each item to an object with only enabled columns
+    const data = items.map(item => {
+      const obj: Record<string, unknown> = {};
+      enabledColumns.forEach(col => {
+        obj[col] = item.original[col as keyof ItemSummary] ?? null;
+      });
+      return obj;
+    });
+
+    const exportObj = {
+      headers: enabledColumns,
+      data,
+    };
+
+    const json = JSON.stringify(exportObj, null, 2);
+    const blob = new Blob([json], { type: "application/json" });
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement("a");
+    a.href = url;
+    a.download = "items.json";
+    a.click();
+    a.remove();
+    URL.revokeObjectURL(url);
+  };
 </script>
 
 <template>
@@ -62,8 +91,11 @@
       </DropdownMenuItem>
       <DropdownMenuItem v-if="multi" @click="console.log('needs to be implemented')"> View items </DropdownMenuItem>
       <DropdownMenuItem @click="$emit('expand')"> Toggle Expand </DropdownMenuItem>
-      <DropdownMenuItem v-if="multi" @click="download(multi.items, multi.columns)">
+      <DropdownMenuItem v-if="multi" @click="downloadCsv(multi.items, multi.columns)">
         Download Table as CSV
+      </DropdownMenuItem>
+      <DropdownMenuItem v-if="multi" @click="downloadJson(multi.items, multi.columns)">
+        Download Table as JSON
       </DropdownMenuItem>
     </DropdownMenuContent>
   </DropdownMenu>
