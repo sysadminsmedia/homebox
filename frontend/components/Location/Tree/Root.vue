018f1f5977e4dc6e0308696863f3452c65185b7f
tonyaellie <tonya@tokia.dev>
2025-12-22 23:34:29 +0000

    fix: use logical sorting for locations

--- frontend/components/Location/Tree/Root.vue
+++ frontend/components/Location/Tree/Root.vue
@@ -7,14 +7,21 @@
     treeId: string;
   };
 
-  defineProps<Props>();
+  const props = defineProps<Props>();
+
+  const collator = new Intl.Collator(undefined, { numeric: true, sensitivity: "base" });
+
+  const sortedLocs = computed(() => {
+    const list = props.locs ?? [];
+    return [...list].sort((a, b) => collator.compare(a.name, b.name));
+  });
 </script>
 
 <template>
   <div>
-    <p v-if="locs.length === 0" class="text-center text-sm">
+    <p v-if="sortedLocs.length === 0" class="text-center text-sm">
       {{ $t("location.tree.no_locations") }}
     </p>
-    <LocationTreeNode v-for="item in locs" :key="item.id" :item="item" :tree-id="treeId" />
+    <LocationTreeNode v-for="item in sortedLocs" :key="item.id" :item="item" :tree-id="treeId" />
   </div>
 </template>
