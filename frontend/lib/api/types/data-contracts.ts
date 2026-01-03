d29a19101a179b87cc63286465a85a2c3b49b03a
Señor Rolando <git@sr-rolando.com>
2026-01-03 20:32:48 +0100

    feat: add 'Purchase Price per Month' to item details

--- frontend/lib/api/types/data-contracts.ts
+++ frontend/lib/api/types/data-contracts.ts
@@ -648,9 +648,10 @@ export interface ItemOut {
   /** Extras */
   notes: string;
   parent?: ItemSummary | null;
+  /** Purchase */
   purchaseFrom: string;
   purchasePrice: number;
-  /** Purchase */
+  purchasePricePerMonth: number;
   purchaseTime: Date | string;
   quantity: number;
   serialNumber: string;
