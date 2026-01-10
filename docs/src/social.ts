e2367f6d209d8b36b306144486533cd2f6a9f13c
Matthew Kilgore <matthew@kilgore.dev>
2026-01-10 10:48:22 -0500

    Initial migration to starlight docs

---
+++ docs/src/social.ts
@@ -0,0 +1,8 @@
+export const socialLinks = [
+    { icon: 'simple-icons:discord', label: 'Discord', href: 'https://discord.homebox.software'},
+    { icon: 'simple-icons:github', label: 'GitHub', href: 'https://git.homebox.software' },
+    { icon: 'simple-icons:mastodon', label: 'Mastodon', href: 'https://noc.social/@sysadminszone'},
+    { icon: 'simple-icons:reddit', label: 'Reddit', href: 'https://reddit.com/r/homebox'},
+    { icon: 'simple-icons:lemmy', label: 'Lemmy', href: 'https://lemmy.world/c/homebox'}
+];
+
