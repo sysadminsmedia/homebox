44eb3ef98bacd19a9e8d6ecb8777431ec9daaeea
Matthew Kilgore <matthew@kilgore.dev>
2026-01-11 16:23:42 -0500

    docs: add code of conduct

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -110,6 +110,10 @@ export default defineConfig({
                                             slug: 'contribute/bounty-program',
                                         },
                                         {
+                                            label: 'Code of Conduct',
+                                            slug: 'contribute/code-of-conduct',
+                                        },
+                                        {
                                             label: 'Development',
                                             autogenerate: { directory: 'contribute/development' },
                                         },
