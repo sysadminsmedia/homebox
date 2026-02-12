bf05e603a2d36f1561209f44b498dc398863f46e
Matthew Kilgore <matthew@kilgore.dev>
2026-02-11 20:24:43 -0500

    fix: postgres timestamp type

--- backend/internal/data/migrations/postgres/20260211200601_template_custom_fields_types.sql
+++ backend/internal/data/migrations/postgres/20260211200601_template_custom_fields_types.sql
@@ -2,4 +2,4 @@
 -- +goose no transaction
 ALTER TABLE template_fields ADD COLUMN number_value INTEGER;
 ALTER TABLE template_fields ADD COLUMN boolean_value BOOLEAN DEFAULT FALSE;
-ALTER TABLE template_fields ADD COLUMN time_value DATETIME;
+ALTER TABLE template_fields ADD COLUMN time_value timestamp;
