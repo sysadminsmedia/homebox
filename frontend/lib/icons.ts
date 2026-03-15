649e52c38a2a5d8e94d0d5529497130938ca73f8
tonyaellie <tonya@tokia.dev>
2026-03-15 12:53:52 +0000

    feat: better icons for tags

--- frontend/lib/icons.ts
+++ frontend/lib/icons.ts
@@ -1,57 +1,40 @@
-import MdiHome from "~icons/mdi/home";
-import MdiRoomService from "~icons/mdi/room-service";
-import MdiChair from "~icons/mdi/chair";
-import MdiLaptop from "~icons/mdi/laptop";
-import MdiWrench from "~icons/mdi/wrench";
-import MdiBook from "~icons/mdi/book";
-import MdiFileCabinet from "~icons/mdi/file-cabinet";
-import MdiDumbbell from "~icons/mdi/dumbbell";
-import MdiBasketball from "~icons/mdi/basketball";
-import MdiGamepad from "~icons/mdi/gamepad";
-import MdiToolbox from "~icons/mdi/toolbox";
-import MdiLightbulb from "~icons/mdi/lightbulb";
-import MdiPlug from "~icons/mdi/plug";
-import MdiCloset from "~icons/mdi/closet";
-import MdiMicrowave from "~icons/mdi/microwave";
-import MdiCoffee from "~icons/mdi/coffee";
 import MdiTagOutline from "~icons/mdi/tag-outline";
-
-export type IconName =
-  | "home"
-  | "room-service"
-  | "chair"
-  | "laptop"
-  | "wrench"
-  | "book"
-  | "file-cabinet"
-  | "dumbbell"
-  | "basketball"
-  | "gamepad"
-  | "toolbox"
-  | "lightbulb"
-  | "plug"
-  | "closet"
-  | "microwave"
-  | "coffee";
+import MdiTreeOutline from "~icons/mdi/tree-outline";
+import MdiBagSuitcaseOutline from "~icons/mdi/bag-suitcase-outline";
+import MdiBedOutline from "~icons/mdi/bed-outline";
+import MdiKitchenCounterOutline from "~icons/mdi/kitchen-counter-outline";
+import MdiBookOpenVariantOutline from "~icons/mdi/book-open-variant-outline";
+import MdiLaptopOutline from "~icons/mdi/laptop";
+import MdiToolboxOutline from "~icons/mdi/toolbox-outline";
+import MdiFileCabinetOutline from "~icons/mdi/folder-outline";
+import MdiDresserOutline from "~icons/mdi/dresser-outline";
+import MdiLightbulbOutline from "~icons/mdi/lightbulb-outline";
+import MdiPowerPlugOutline from "~icons/mdi/power-plug-outline";
+import MdiWrenchOutline from "~icons/mdi/wrench-outline";
+import MdiDumbbellOutline from "~icons/mdi/dumbbell";
+import MdiSofaOutline from "~icons/mdi/sofa-outline";
+import MdiPalleteOutline from "~icons/mdi/palette-outline";
 
 export const availableIcons = [
-  { name: "home", component: MdiHome },
-  { name: "room-service", component: MdiRoomService },
-  { name: "chair", component: MdiChair },
-  { name: "laptop", component: MdiLaptop },
-  { name: "wrench", component: MdiWrench },
-  { name: "book", component: MdiBook },
-  { name: "file-cabinet", component: MdiFileCabinet },
-  { name: "dumbbell", component: MdiDumbbell },
-  { name: "basketball", component: MdiBasketball },
-  { name: "gamepad", component: MdiGamepad },
-  { name: "toolbox", component: MdiToolbox },
-  { name: "lightbulb", component: MdiLightbulb },
-  { name: "plug", component: MdiPlug },
-  { name: "closet", component: MdiCloset },
-  { name: "microwave", component: MdiMicrowave },
-  { name: "coffee", component: MdiCoffee },
+  { name: "tag-outline", component: MdiTagOutline },
+  { name: "tree-outline", component: MdiTreeOutline },
+  { name: "bag-suitcase-outline", component: MdiBagSuitcaseOutline },
+  { name: "bed-outline", component: MdiBedOutline },
+  { name: "kitchen-counter-outline", component: MdiKitchenCounterOutline },
+  { name: "book-open-variant-outline", component: MdiBookOpenVariantOutline },
+  { name: "laptop", component: MdiLaptopOutline },
+  { name: "sofa-outline", component: MdiSofaOutline },
+  { name: "toolbox-outline", component: MdiToolboxOutline },
+  { name: "file-cabinet-outline", component: MdiFileCabinetOutline },
+  { name: "dresser-outline", component: MdiDresserOutline },
+  { name: "lightbulb-outline", component: MdiLightbulbOutline },
+  { name: "power-plug-outline", component: MdiPowerPlugOutline },
+  { name: "wrench-outline", component: MdiWrenchOutline },
+  { name: "dumbbell", component: MdiDumbbellOutline },
+  { name: "palette-outline", component: MdiPalleteOutline },
 ] as const;
+
+export type IconName = (typeof availableIcons)[number]["name"];
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function getIconComponent(iconName: string | undefined): any {
